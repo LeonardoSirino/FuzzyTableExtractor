5fe9596d377a69c985a1df627e2e3860594ea2fe
Leonardo Sirino <leonardosirino@outlook.com>
2022-01-23 13:20:12 -0300

    Mocked some imports to enable the docs building

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,6 +16,8 @@ import sys
 sys.path.insert(0, os.path.abspath("."))
 sys.path.insert(0, os.path.abspath("../.."))
 
+autodoc_mock_imports = ["pywin32", "fuzzywuzzy", "pandas", "python-docx", "numpy"]
+
 
 # -- Project information -----------------------------------------------------
 
